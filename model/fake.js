fc055d8988dac91342d23d04030bae75ff671d72
Shiang-Yu Huang <sky90456@hotmail.com>
2018-10-18 19:44:11 +0800

    模擬form data using querystring

--- model/fake.js
+++ model/fake.js
@@ -1,5 +1,6 @@
 const Random = require('./tw-random')
 
+const querystring = require('querystring');
 
 const axios = require('axios')
 
@@ -34,7 +35,6 @@ const data2 = {
   backurl: 'http://www.nwau.net/twpbdn/?cid=QQ&fbclid=IwAR2Xgfy-HWzeKTlvoj_OfvrRglp8oObChIUXWbOuR9Iu_Kl2tzm8-c3eWz8',
   fromurl: 'http://www.nwau.net/twpbdn/?cid=QQ&fbclid=IwAR2Xgfy-HWzeKTlvoj_OfvrRglp8oObChIUXWbOuR9Iu_Kl2tzm8-c3eWz8',
   cid: 'QQ',
-  bm:'',
   num: 1,
   proname: '白色平板 | 白色鼠标 | 蓝牙耳机 | 指环扣',
   productpackage: '白色平板 + 白色鼠标 + 蓝牙耳机 + 指环扣',
@@ -42,12 +42,15 @@ const data2 = {
   username: Random.getRandomName(),
   mobile: Random.getRandomPhone(),
   cusprovince: '台灣',
-  cuscity: '宜蘭縣',
-  cusarea: '冬山鄉',
+  cuscity: '台北市',
+  cusarea: '大同區',
   addr: Random.getRandomAddress(),
-  postal: 0,
-  mail:Random.getRandomEmail()
+  postal: 000000,
+  mail:Random.getRandomEmail(),
+  addr: Random.getRandomAddress()
 }
+
+
 
 
 
@@ -59,25 +62,14 @@ const attack = async (proxy) => {
 
   try {
 
-    //http://t.beds-ym.com/shop/?m=Show&a=booking
-    //http://post.odaq.cn/order_hwd.php
     const postData = {
       method: 'post',
       url: 'http://post.odaq.cn/order_hwd.php',
-      data: data2,
-      transformRequest: [() => {
-        let ret = ''
-        for (let it in data2) {
-          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-        }
-        // console.log(ret)
-        return ret
-      }],
+      data: querystring.stringify(data2),
       headers: { 
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36(KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
-        'Accept': 'text / plain',
-        'Origin':'http://www.nwau.net'
+        'Accept': 'text / plain'
       },
       
     }
@@ -93,7 +85,7 @@ const attack = async (proxy) => {
     if (!result.data) {return 'no data string response'}
 
     
-    
+    console.log(result.data)
 
     if (typeof result.data === 'object' ) {
       return 'ok'
@@ -129,3 +121,22 @@ module.exports.attack = attack
 
 
 
+// const postData = {
+//   method: 'post',
+//   url: 'http://post.odaq.cn/order_hwd.php',
+//   data: data2,
+//   transformRequest: [() => {
+//     let ret = ''
+//     for (let it in data2) {
+//       ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+//     }
+//     // console.log(ret)
+//     return ret
+//   }],
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+//     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36(KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+//     'Accept': 'text / plain'
+//   },
+
+// }
